3c1278d6a23057c61e639dada49e469840059185
fbzr <fabricio.spb@gmail.com>
2020-08-04 09:16:12 -0700

    change favicon, delete unused images, update metadata description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Commercial Cannabis Inc.`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Commercial Cannabis (Inc) is a Wholesale Marketing company. Using its nationally spread force, Commercial Cannabis Inc matches each type of business and products with the right partner(s). We introduce good people in the cannabis industry and ‘cultivate’ business to business relations.`,
+    author: `@jamesbellile`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -46,10 +46,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6ebe46`,
+        theme_color: `#6ebe46`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/commercial-cannabis-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
